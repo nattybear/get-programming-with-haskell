8b89e3faa41885359650e35902235d9fd335c30c
준규 <vpark45@gmail.com>
2022-01-04 00:36:17 +0900

    Defining the createEmail function

--- lesson1/first_prog.hs
+++ lesson1/first_prog.hs
@@ -13,3 +13,7 @@ messyMain = do
 toPart recipient = "Dear " ++ recipient ++ ",\n"
 bodyPart bookTitle = "Thanks for buying " ++ bookTitle ++ ",\n";
 fromPart author = "Thanks, \n"++author
+
+createEmail recipient bookTitle author = toPart recipient ++
+                                         bodyPart bookTitle ++
+                                         fromPart author
