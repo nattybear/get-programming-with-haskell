88efac75ebbda2958ba2c6650164c8af5c3926f9
준규 <vpark45@gmail.com>
2022-01-06 07:26:18 +0900

    Modify to use lambda function

--- unit1/lesson3/listing3.1.hs
+++ unit1/lesson3/listing3.1.hs
@@ -1,5 +1,4 @@
-body sumSquare  squareSum = if sumSquare > squareSum
-                            then sumSquare
-                            else squareSum
-
-sumSquareOrSquareSum x y = body (x^2 + y^2) ((x+y)^2)
+sumSquareOrSquareSum x y = (\sumSquare squareSum ->
+                             if sumSquare > squareSum
+                             then sumSquare
+                             else squareSum) (x^2 + y^2) ((x+y)^2)
