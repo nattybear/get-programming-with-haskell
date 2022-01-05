529d5db484552063d9043d2cb155fbba625a543a
준규 <vpark45@gmail.com>
2022-01-06 07:23:25 +0900

    Add function body

--- unit1/lesson3/listing3.1.hs
+++ unit1/lesson3/listing3.1.hs
@@ -1,5 +1,5 @@
-sumSquareOrSquareSum x y = if sumSquare > squareSum
-                           then sumSquare
-                           else squareSum
-  where sumSquare = x^2 + y^2
-        squareSum = (x+y)^2
+body sumSquare  squareSum = if sumSquare > squareSum
+                            then sumSquare
+                            else squareSum
+
+sumSquareOrSquareSum x y = body (x^2 + y^2) ((x+y)^2)
