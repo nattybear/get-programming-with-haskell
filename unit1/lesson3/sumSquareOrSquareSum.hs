179a872d91d36e0f9ebe53a65814935d9a4e765f
준규 <vpark45@gmail.com>
2022-01-06 07:45:42 +0900

    Update using let expression

--- unit1/lesson3/sumSquareOrSquareSum.hs
+++ unit1/lesson3/sumSquareOrSquareSum.hs
@@ -1,4 +1,6 @@
-sumSquareOrSquareSum x y = (\sumSquare squareSum ->
-                             if sumSquare > squareSum
-                             then sumSquare
-                             else squareSum) (x^2 + y^2) ((x+y)^2)
+sumSquareOrSquareSum x y = let sumSquare = (x^2 + y^2)
+                               squareSum = (x+y)^2
+                           in
+                            if sumSquare > squareSum
+                            then sumSquare
+                            else squareSum
