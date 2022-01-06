7ff57f83bee8a82588f03c83132e47f8a15ca58a
준규 <vpark45@gmail.com>
2022-01-06 18:23:24 +0900

    Modify to use IIFE

--- unit1/lesson3/lexical.js
+++ unit1/lesson3/lexical.js
@@ -3,8 +3,10 @@ var libraryAdd = function(a, b) {
   return c;
 }
 
-var a = 2;
-var b = 3;
-var c = a + b;
-var d = libraryAdd(10, 20);
-console.log(c);
+var x = (function() {
+  var a = 2;
+  var b = 3;
+  var c = a + b;
+  var d = libraryAdd(10, 20);
+  console.log(c);
+})()
