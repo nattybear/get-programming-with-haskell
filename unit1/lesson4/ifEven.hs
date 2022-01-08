3961f10db330ff2f206bacb47cb1d1dd4df75e7e
준규 <vpark45@gmail.com>
2022-01-09 08:50:13 +0900

    Modify code for function takes function as argument

--- unit1/lesson4/ifEven.hs
+++ unit1/lesson4/ifEven.hs
@@ -1,11 +1,3 @@
-ifEvenInc n = if even n
-              then n + 1
-              else  n
-
-ifEvenDouble n = if even n
-                 then n * 2
-                 else n
-
-ifEvenSquare n = if even n
-                 then n^2
-                 else n
+ifEven myFunction x = if even x
+                      then myFunction x
+                      else x
