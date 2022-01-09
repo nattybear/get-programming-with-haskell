3563ae78a65bcc7ec6e8fea42c7a97e07fada7a5
준규 <vpark45@gmail.com>
2022-01-09 09:09:48 +0900

    Fix to compare first name when last name is identical

--- unit1/lesson4/compareNames.hs
+++ unit1/lesson4/compareNames.hs
@@ -9,6 +9,12 @@ compareLastNames name1 name2 = if lastName1 > lastName2
                                then GT
                                else if lastName1 < lastName2
                                     then LT
-                                    else EQ
+                                    else if firstName1 > firstName2
+                                         then GT
+                                         else if firstName1 < firstName2
+                                              then LT
+                                              else EQ
   where lastName1 = snd name1
         lastName2 = snd name2
+        firstName1 = fst name1
+        firstName2 = fst name2
