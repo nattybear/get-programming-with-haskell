73c86d34ba758e77f49025b99be727ffb978a977
준규 <vpark45@gmail.com>
2022-01-09 09:28:54 +0900

    Rewrite compareLastNames by using compare

--- unit1/lesson4/compareNames.hs
+++ unit1/lesson4/compareNames.hs
@@ -5,15 +5,10 @@ names = [("Ian", "Curtis"),
          ("Peter", "Hook"),
          ("Stephen", "Morris")]
 
-compareLastNames name1 name2 = if lastName1 > lastName2
-                               then GT
-                               else if lastName1 < lastName2
-                                    then LT
-                                    else if firstName1 > firstName2
-                                         then GT
-                                         else if firstName1 < firstName2
-                                              then LT
-                                              else EQ
+compareLastNames name1 name2 =
+  case compare lastName1 lastName2 of
+    EQ -> compare firstName1 firstName2
+    v  -> v
   where lastName1 = snd name1
         lastName2 = snd name2
         firstName1 = fst name1
