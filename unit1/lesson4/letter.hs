b52aa4943dff2e5180d03dd8bd2c4c6239cc7011
준규 <vpark45@gmail.com>
2022-01-09 09:19:43 +0900

    Add getLocationFunction

--- unit1/lesson4/letter.hs
+++ unit1/lesson4/letter.hs
@@ -14,3 +14,9 @@ nyOffice name = nameText ++ ": PO Box 789 - New York, NY, 10013"
 
 renoOffice name = nameText ++ " - PO Box 456 - Reno, NV 89523"
   where nameText = snd name
+
+getLocationFunction location = case location of
+  "ny" -> nyOffice
+  "sf" -> sfOffice
+  "reno" -> renoOffice
+  _ -> (\name -> (fst name) ++ " " ++ (snd name))
