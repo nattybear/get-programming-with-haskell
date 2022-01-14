8c4e6e39badf42993e28bfe9fd46c498abc9337a
준규 <vpark45@gmail.com>
2022-01-15 01:16:32 +0900

    Add genApiRequestBuilder

--- unit1/lesson5/getRequestURL.hs
+++ unit1/lesson5/getRequestURL.hs
@@ -10,3 +10,6 @@ genHostRequestBuilder host = (\apiKey resource id ->
                                getRequestURL host apiKey resource id)
 
 exampleUrlBuilder = genHostRequestBuilder "http://example.com"
+
+genApiRequestBuilder hostBuilder apiKey = (\resource id ->
+                                            hostBuilder apiKey resource id)
