0637d3d545a2fd64b1ee47944c87a48c5eefc0df
준규 <vpark45@gmail.com>
2022-01-27 06:29:31 +0900

    Change the priority of attack

--- unit1/lesson10/robot.hs
+++ unit1/lesson10/robot.hs
@@ -42,9 +42,9 @@ killerRobotRound3 = fight gentleGiantRound2 killerRobotRound2
 fastRobot = robot ("speedy", 15, 40)
 slowRobot = robot ("slowpoke",20,30)
 
-fastRobotRound1 = fight slowRobot fastRobot
 slowRobotRound1 = fight fastRobot slowRobot
-fastRobotRound2 = fight slowRobotRound1 fastRobotRound1
+fastRobotRound1 = fight slowRobotRound1 fastRobot
 slowRobotRound2 = fight fastRobotRound1 slowRobotRound1
-fastRobotRound3 = fight slowRobotRound2 fastRobotRound2
-slowRobotRound3 = fight fastRobotRound2 slowRobotRound2
+slowRobotRound3 = fight fastRobotRound1 slowRobotRound2
+slowRobotRound4 = fight fastRobotRound1 slowRobotRound3
+fastRobotRound2 = fight slowRobotRound4 fastRobotRound1
