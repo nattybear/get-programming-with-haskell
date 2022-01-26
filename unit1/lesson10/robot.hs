fe210ef28903218e2404da96ad1e1fcb774e1e37
준규 <vpark45@gmail.com>
2022-01-27 05:46:05 +0900

    Write some setters

--- unit1/lesson10/robot.hs
+++ unit1/lesson10/robot.hs
@@ -9,3 +9,7 @@ hp (_,_,hp) = hp
 getName aRobot = aRobot name
 getAttack aRobot = aRobot attack
 getHp aRobot = aRobot hp
+
+setName aRobot newName = aRobot (\(n,a,h) -> robot (newName,a,h))
+setAttack aRobot newAttack = aRobot (\(n,a,h) -> robot (n,newAttack,h))
+setHp aRobot newHP = aRobot (\(n,a,h) -> robot (n,a,newHP))
