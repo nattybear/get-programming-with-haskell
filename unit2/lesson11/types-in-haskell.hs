a914cbb26646f8d6f508d59099bc40e12618c16f
준규 <vpark45@gmail.com>
2022-01-28 20:56:27 +0900

    List types

--- unit2/lesson11/types-in-haskell.hs
+++ unit2/lesson11/types-in-haskell.hs
@@ -12,3 +12,12 @@ interestRate = 0.375
 
 isFun :: Bool
 isFun = True
+
+values :: [Int]
+values = [1,2,3]
+
+testScores :: [Double]
+testScores = [0.99,0.7,0.8]
+
+letters :: [Char]
+letters = ['a','b','c']
