da89898b09cf882101b4a5069fd261cc0000fc07
준규 <vpark45@gmail.com>
2022-01-28 21:00:23 +0900

    Tuple types

--- unit2/lesson11/types-in-haskell.hs
+++ unit2/lesson11/types-in-haskell.hs
@@ -27,3 +27,12 @@ aPet = "cat"
 
 anotherPet :: String
 anotherPet = "dog"
+
+ageAndHeight :: (Int,Int)
+ageAndHeight = (34,74)
+
+firstLastMiddle :: (String,String,Char)
+firstLastMiddle = ("Oscar","Grouch",'D')
+
+streetAddress :: (Int,String)
+streetAddress = (123,"Happy St.")
