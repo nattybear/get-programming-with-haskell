df6afb4c3bed49b6caeee315d865221a87e0702e
준규 <vpark45@gmail.com>
2022-01-31 20:25:33 +0900

    Create a few helper functions

--- unit2/lesson12/using-type-synonyms.hs
+++ unit2/lesson12/using-type-synonyms.hs
@@ -8,3 +8,9 @@ patientInfo :: FirstName -> LastName -> Age -> Height -> String
 patientInfo fname lname age height = name ++ " " ++ ageHeight
   where name = lname ++ ", " ++ fname
         ageHeight = "(" ++ show age ++ "yrs. " ++ show height ++ "in.)"
+
+firstName :: PatientName -> String
+firstName patient = fst patient
+
+lastName :: PatientName -> String
+lastName patient = snd patient
