21b6e929ec37de3e500098652183ba3b0d06f177
준규 <vpark45@gmail.com>
2022-01-31 20:21:55 +0900

    Type synonyms

--- unit2/lesson12/using-type-synonyms.hs
+++ unit2/lesson12/using-type-synonyms.hs
@@ -1,3 +1,8 @@
+type FirstName = String
+type LastName = String
+type Age = Int
+type Height = Int
+
 patientInfo :: String -> String -> Int -> Int -> String
 patientInfo fname lname age height = name ++ " " ++ ageHeight
   where name = lname ++ ", " ++ fname
