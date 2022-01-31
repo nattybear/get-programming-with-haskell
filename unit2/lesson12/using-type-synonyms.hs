dd1f7e28539c6f0cd5624900ba11815d5a22a233
준규 <vpark45@gmail.com>
2022-01-31 20:24:26 +0900

    Add type synonym: PatientName

--- unit2/lesson12/using-type-synonyms.hs
+++ unit2/lesson12/using-type-synonyms.hs
@@ -2,6 +2,7 @@ type FirstName = String
 type LastName = String
 type Age = Int
 type Height = Int
+type PatientName = (String,String)
 
 patientInfo :: FirstName -> LastName -> Age -> Height -> String
 patientInfo fname lname age height = name ++ " " ++ ageHeight
