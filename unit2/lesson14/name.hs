59ad5f5954db912bccdda3efffdc8347ce31e605
준규 <vpark45@gmail.com>
2022-02-10 22:20:49 +0900

    Define a new type Name using data

--- unit2/lesson14/name.hs
+++ unit2/lesson14/name.hs
@@ -1,9 +1,1 @@
-type Name = (String,String)
-
-names :: [Name]
-names = [ ("Emil","Cioran")
-        , ("Eugene","Thacker")
-        , ("Friedrich","Nietzsche")]
-
-instance Ord Name where
-  compare (f1,l1) (f2,l2) = compare (l1,f1) (l2,f2)
+data Name = Name (String, String) deriving (Show, Eq)
