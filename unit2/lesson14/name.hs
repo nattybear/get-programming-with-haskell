c0e9998ac8335343d1e9061f98beef777faa0e28
준규 <vpark45@gmail.com>
2022-02-10 22:24:03 +0900

    Add names

--- unit2/lesson14/name.hs
+++ unit2/lesson14/name.hs
@@ -2,3 +2,8 @@ data Name = Name (String, String) deriving (Show, Eq)
 
 instance Ord Name where
   compare (Name (f1,l1)) (Name (f2,l2)) = compare (l1,f1) (l2,f2)
+
+names :: [Name]
+names = [ Name ("Emil","Cioran")
+        , Name ("Eugene","Thacker")
+        , Name ("Friedrich","Nietzsche") ]
