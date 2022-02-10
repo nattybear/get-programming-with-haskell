e8e0e050fd7eba1f350cfd562d53c5da3f47367a
준규 <vpark45@gmail.com>
2022-02-10 22:17:08 +0900

    Attempt to implement Ord for a type synonym

--- unit2/lesson14/name.hs
+++ unit2/lesson14/name.hs
@@ -4,3 +4,6 @@ names :: [Name]
 names = [ ("Emil","Cioran")
         , ("Eugene","Thacker")
         , ("Friedrich","Nietzsche")]
+
+instance Ord Name where
+  compare (f1,l1) (f2,l2) = compare (l1,f1) (l2,f2)
