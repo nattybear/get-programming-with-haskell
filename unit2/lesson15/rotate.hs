0804260b4ed28c6ce26cf310c2794eb77dd7be28
준규 <vpark45@gmail.com>
2022-02-13 23:53:26 +0900

    Add rotEncoder and rotDecoder and reorder functions

--- unit2/lesson15/rotate.hs
+++ unit2/lesson15/rotate.hs
@@ -17,11 +17,6 @@ rotChar charToEncrypt = rotN sizeOfAlphabet charToEncrypt
 message :: [FourLetterAlphabet]
 message = [L1,L3,L4,L1,L1,L2]
 
-fourLetterAlphabetEncoder :: [FourLetterAlphabet] -> [FourLetterAlphabet]
-fourLetterAlphabetEncoder vals = map rot4l vals
-  where alphaSize = 1 + fromEnum (maxBound :: FourLetterAlphabet)
-        rot4l = rotN alphaSize
-
 data ThreeLetterAlphabet = Alpha
                          | Beta
                          | Kappa deriving (Show,Enum,Bounded)
@@ -29,10 +24,6 @@ data ThreeLetterAlphabet = Alpha
 threeLetterMessage :: [ThreeLetterAlphabet]
 threeLetterMessage = [Alpha,Alpha,Beta,Alpha,Kappa]
 
-threeLetterEncoder :: [ThreeLetterAlphabet] -> [ThreeLetterAlphabet]
-threeLetterEncoder vals = map rot3l vals
-  where alphaSize = 1 + fromEnum (maxBound :: ThreeLetterAlphabet)
-        rot3l = rotN alphaSize
 
 rotNdecoder :: (Bounded a, Enum a) => Int -> a -> a
 rotNdecoder n c = toEnum rotation
@@ -42,7 +33,33 @@ rotNdecoder n c = toEnum rotation
                  else 1 + fromEnum c + halfN
         rotation = offset `mod` n
 
+
+rotEncoder :: String -> String
+rotEncoder text = map rotChar text
+  where alphaSize = 1 + fromEnum (maxBound :: Char)
+        rotChar = rotN alphaSize
+
+rotDecoder :: String -> String
+rotDecoder text = map rotCharDecoder text
+  where alphaSize = 1 + fromEnum (maxBound :: Char)
+        rotCharDecoder = rotNdecoder alphaSize
+
+threeLetterEncoder :: [ThreeLetterAlphabet] -> [ThreeLetterAlphabet]
+threeLetterEncoder vals = map rot3l vals
+  where alphaSize = 1 + fromEnum (maxBound :: ThreeLetterAlphabet)
+        rot3l = rotN alphaSize
+
 threeLetterDecoder :: [ThreeLetterAlphabet] -> [ThreeLetterAlphabet]
 threeLetterDecoder vals = map rot3ldecoder vals
   where alphaSize = 1 + fromEnum (maxBound :: ThreeLetterAlphabet)
         rot3ldecoder = rotNdecoder alphaSize
+
+fourLetterAlphabetEncoder :: [FourLetterAlphabet] -> [FourLetterAlphabet]
+fourLetterAlphabetEncoder vals = map rot4l vals
+  where alphaSize = 1 + fromEnum (maxBound :: FourLetterAlphabet)
+        rot4l = rotN alphaSize
+
+fourLetterDecoder :: [FourLetterAlphabet] -> [FourLetterAlphabet]
+fourLetterDecoder vals = map rot4ldecoder vals
+  where alphaSize = 1 + fromEnum (maxBound :: FourLetterAlphabet)
+        rot4ldecoder = rotNdecoder alphaSize
