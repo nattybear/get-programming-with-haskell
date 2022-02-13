54f533c59354576615508830a064a1ec4447efd0
준규 <vpark45@gmail.com>
2022-02-13 23:46:10 +0900

    Add threeLetterDecoder function

--- unit2/lesson15/rotate.hs
+++ unit2/lesson15/rotate.hs
@@ -33,3 +33,16 @@ threeLetterEncoder :: [ThreeLetterAlphabet] -> [ThreeLetterAlphabet]
 threeLetterEncoder vals = map rot3l vals
   where alphaSize = 1 + fromEnum (maxBound :: ThreeLetterAlphabet)
         rot3l = rotN alphaSize
+
+rotNdecoder :: (Bounded a, Enum a) => Int -> a -> a
+rotNdecoder n c = toEnum rotation
+  where halfN = n `div` 2
+        offset = if even n
+                 then fromEnum c + halfN
+                 else 1 + fromEnum c + halfN
+        rotation = offset `mod` n
+
+threeLetterDecoder :: [ThreeLetterAlphabet] -> [ThreeLetterAlphabet]
+threeLetterDecoder vals = map rot3ldecoder vals
+  where alphaSize = 1 + fromEnum (maxBound :: ThreeLetterAlphabet)
+        rot3ldecoder = rotNdecoder alphaSize
