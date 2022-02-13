ebd2364167d2e8a08e1d0a14e994c264292dd295
준규 <vpark45@gmail.com>
2022-02-13 23:24:59 +0900

    Add fourLetterAlphabetEncoder function

--- unit2/lesson15/rotate.hs
+++ unit2/lesson15/rotate.hs
@@ -16,3 +16,8 @@ rotChar charToEncrypt = rotN sizeOfAlphabet charToEncrypt
 
 message :: [FourLetterAlphabet]
 message = [L1,L3,L4,L1,L1,L2]
+
+fourLetterAlphabetEncoder :: [FourLetterAlphabet] -> [FourLetterAlphabet]
+fourLetterAlphabetEncoder vals = map rot4l vals
+  where alphaSize = 1 + fromEnum (maxBound :: FourLetterAlphabet)
+        rot4l = rotN alphaSize
