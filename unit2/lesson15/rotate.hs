904764a929da027c00ca63547f0012a6362ace21
준규 <vpark45@gmail.com>
2022-02-13 23:22:14 +0900

    Add message

--- unit2/lesson15/rotate.hs
+++ unit2/lesson15/rotate.hs
@@ -13,3 +13,6 @@ largestCharNumber = fromEnum (maxBound :: Char)
 rotChar :: Char -> Char
 rotChar charToEncrypt = rotN sizeOfAlphabet charToEncrypt
   where sizeOfAlphabet = 1 + fromEnum (maxBound :: Char)
+
+message :: [FourLetterAlphabet]
+message = [L1,L3,L4,L1,L1,L2]
