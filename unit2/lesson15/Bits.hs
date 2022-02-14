2aa519d29962b01896e87f5a54f6549348c3528a
준규 <vpark45@gmail.com>
2022-02-14 22:13:57 +0900

    Add intToBits' function

--- unit2/lesson15/Bits.hs
+++ unit2/lesson15/Bits.hs
@@ -1,1 +1,10 @@
 type Bits = [Bool]
+
+intToBits' :: Int -> Bits
+intToBits' 0 = [False]
+intToBits' 1 = [True]
+intToBits' n = if (remainder == 0)
+               then False : intToBits' nextVal
+               else True : intToBits' nextVal
+  where remainder = n `mod` 2
+        nextVal = n `div` 2
