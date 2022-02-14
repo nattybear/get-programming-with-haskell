e113e524763a3d173b8c180f6206a98ae28d08d1
준규 <vpark45@gmail.com>
2022-02-14 22:10:02 +0900

    Add xor function

--- unit2/lesson15/xor.hs
+++ unit2/lesson15/xor.hs
@@ -3,3 +3,6 @@ xorBool value1 value2 = (value1 || value2) && (not (value1 && value2))
 
 xorPair :: (Bool,Bool) -> Bool
 xorPair (v1,v2) = xorBool v1 v2
+
+xor :: [Bool] -> [Bool] -> [Bool]
+xor list1 list2 = map xorPair (zip list1 list2)
