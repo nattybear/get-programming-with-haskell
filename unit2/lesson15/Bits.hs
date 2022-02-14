841ef4fdeb5d8e8be3ee93529ded5f7fe3759c38
준규 <vpark45@gmail.com>
2022-02-14 22:19:51 +0900

    Add maxBits and intToBits function

--- unit2/lesson15/Bits.hs
+++ unit2/lesson15/Bits.hs
@@ -8,3 +8,12 @@ intToBits' n = if (remainder == 0)
                else True : intToBits' nextVal
   where remainder = n `mod` 2
         nextVal = n `div` 2
+
+maxBits :: Int
+maxBits = length (intToBits' maxBound)
+
+intToBits :: Int -> Bits
+intToBits n = leadingFalses ++ reversedBits
+  where reversedBits = reverse (intToBits' n)
+        missingBits = maxBits - (length reversedBits)
+        leadingFalses = take missingBits (cycle [False])
