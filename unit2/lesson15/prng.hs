e7d21356bb3b5f81a66095eb511336f4795921dd
준규 <vpark45@gmail.com>
2022-02-15 00:08:31 +0900

    Add examplePRNG function

--- unit2/lesson15/prng.hs
+++ unit2/lesson15/prng.hs
@@ -1,2 +1,9 @@
+import Cipher
+
+data StreamCipher
+
 prng :: Int -> Int -> Int -> Int -> Int
 prng a b maxNumber seed = (a*seed + b) `mod` maxNumber
+
+examplePRNG :: Int -> Int
+examplePRNG = prng 1337 7 100
