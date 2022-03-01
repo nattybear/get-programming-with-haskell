e04d1289fe835f4da5f7c046bfe99141305edec4
준규 <vpark45@gmail.com>
2022-03-01 19:01:40 +0900

    Add createPTable function

--- unit3/lesson17/Coin.hs
+++ unit3/lesson17/Coin.hs
@@ -2,3 +2,8 @@ type Events = [String]
 type Probs = [Double]
 
 data PTable = PTable Events Probs
+
+createPTable :: Events -> Probs -> PTable
+createPTable events probs = PTable events normalizedProbs
+  where totalProbs = sum probs
+        normalizedProbs = map (\x -> x/totalProbs) probs
