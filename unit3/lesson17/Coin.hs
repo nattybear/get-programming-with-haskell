4d69e5ed20e7f68dcb9ab72374ea6a8f825bb029
준규 <vpark45@gmail.com>
2022-03-01 19:11:55 +0900

    Add cartCombine function

--- unit3/lesson17/Coin.hs
+++ unit3/lesson17/Coin.hs
@@ -14,3 +14,10 @@ showPair event prob = mconcat [event, "|", show prob, "\n"]
 instance Show PTable where
   show (PTable events probs) = mconcat pairs
     where pairs = zipWith showPair events probs
+
+cartCombine :: (a -> b -> c) -> [a] -> [b] -> [c]
+cartCombine func l1 l2 = zipWith func newL1 cycledL2
+  where nToAdd = length l2
+        repeatedL1 = map (take nToAdd . repeat) l1
+        newL1 = mconcat repeatedL1
+        cycledL2 = cycle l2
