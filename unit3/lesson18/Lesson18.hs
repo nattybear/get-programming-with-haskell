57eb9b40ba3da60b3c1b51fd147017453cbba1e1
준규 <vpark45@gmail.com>
2022-03-16 23:23:23 +0900

    Compare my List to the built-in list

--- unit3/lesson18/Lesson18.hs
+++ unit3/lesson18/Lesson18.hs
@@ -39,3 +39,15 @@ transform :: (a -> a) -> Triple a -> Triple a
 transform f (Triple x y z) = Triple (f x) (f y) (f z)
 
 data List a = Empty | Cons a (List a) deriving Show
+
+builtinEx1 :: [Int]
+builtinEx1 = 1:2:3:[]
+
+ourListEx1 :: List Int
+ourListEx1 = Cons 1 (Cons 2 (Cons 3 Empty))
+
+builtinEx2 :: [Char]
+builtinEx2 = 'c':'a':'t':[]
+
+ourListEx2 :: List Char
+ourListEx2 = Cons 'c' (Cons 'a' (Cons 't' Empty))
