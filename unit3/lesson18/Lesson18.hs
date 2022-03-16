a6cfb4f0e26f77f712d3b5ae7fd865de3543f241
준규 <vpark45@gmail.com>
2022-03-16 20:31:55 +0900

    Assessors for the Triple type

--- unit3/lesson18/Lesson18.hs
+++ unit3/lesson18/Lesson18.hs
@@ -22,3 +22,12 @@ type Initials = Triple Char
 
 initials :: Initials
 initials = Triple 'H' 'P' 'L'
+
+first :: Triple a -> a
+first (Triple x _ _) = x
+
+second :: Triple a -> a
+second (Triple _ x _) = x
+
+third :: Triple a -> a
+third (Triple _ _ x) = x
