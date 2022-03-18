dfd306b20a5482c912741814a7670ff1720fbf2f
준규 <vpark45@gmail.com>
2022-03-19 01:09:31 +0900

    A list of availableOrgans that can contain missing values

--- unit3/lesson19/Lesson19.hs
+++ unit3/lesson19/Lesson19.hs
@@ -27,3 +27,6 @@ possibleDrawers = [1 .. 50]
 getDrawerContents :: [Int] -> Map.Map Int Organ -> [Maybe Organ]
 getDrawerContents ids catalog = map getContents ids
   where getContents = \id -> Map.lookup id catalog
+
+availableOrgans :: [Maybe Organ]
+availableOrgans = getDrawerContents possibleDrawers organCatalog
