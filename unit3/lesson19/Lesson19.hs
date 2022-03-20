ab7786e483b3851bcbd6b700f3bca4e7d479ec99
준규 <vpark45@gmail.com>
2022-03-20 21:45:10 +0900

    Solve Summary Q19.2

--- unit3/lesson19/Lesson19.hs
+++ unit3/lesson19/Lesson19.hs
@@ -99,3 +99,7 @@ processAndReport Nothing = "error, id not found"
 
 emptyDrawers :: [Maybe Organ] -> Int
 emptyDrawers = length . filter isNothing
+
+maybeMap :: (a -> b) -> Maybe a -> Maybe b
+maybeMap _ Nothing = Nothing
+maybeMap f (Just x) = Just (f x)
