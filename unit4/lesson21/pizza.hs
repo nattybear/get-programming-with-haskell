34177081c30f21fe4e9de5ae170d04a0be7d3626
준규 <vpark45@gmail.com>
2022-04-01 10:40:57 +0900

    Add main action

--- unit4/lesson21/pizza.hs
+++ unit4/lesson21/pizza.hs
@@ -19,3 +19,18 @@ describePizza (size,cost) = "The " ++ show size ++ " pizza " ++
                             show costSqInch ++
                             " per square inch"
   where costSqInch = costPerInch (size,cost)
+
+main :: IO ()
+main = do
+  putStrLn "What is the size of pizza 1"
+  size1 <- getLine
+  putStrLn "What is the cost of pizza 1"
+  cost1 <- getLine
+  putStrLn "What is the size of pizza 2"
+  size2 <- getLine
+  putStrLn "What is the cost of pizza 2"
+  cost2 <- getLine
+  let pizza1 = (read size1, read cost1)
+  let pizza2 = (read size2, read cost2)
+  let betterPizza = comparePizzas pizza1 pizza2
+  putStrLn (describePizza betterPizza)
