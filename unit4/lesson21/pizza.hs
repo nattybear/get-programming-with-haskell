0530478173b5c99a6805330c1b6868c349e6a3fc
준규 <vpark45@gmail.com>
2022-04-01 10:24:40 +0900

    Add costPerInch function

--- unit4/lesson21/pizza.hs
+++ unit4/lesson21/pizza.hs
@@ -2,3 +2,6 @@ areaGivenDiameter :: Double -> Double
 areaGivenDiameter size = pi * (size / 2) ^ 2
 
 type Pizza = (Double,Double)
+
+costPerInch :: Pizza -> Double
+costPerInch (size, cost) = cost / areaGivenDiameter size
