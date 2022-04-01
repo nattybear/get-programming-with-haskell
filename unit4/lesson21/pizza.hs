191b0c4206d7f251c5f4e6a501ad89e10ed4f85f
준규 <vpark45@gmail.com>
2022-04-01 10:33:37 +0900

    Add comparePizzas function

--- unit4/lesson21/pizza.hs
+++ unit4/lesson21/pizza.hs
@@ -5,3 +5,10 @@ type Pizza = (Double,Double)
 
 costPerInch :: Pizza -> Double
 costPerInch (size, cost) = cost / areaGivenDiameter size
+
+comparePizzas :: Pizza -> Pizza -> Pizza
+comparePizzas p1 p2 = if costP1 < costP2
+                      then p1
+                      else p2
+  where costP1 = costPerInch p1
+        costP2 = costPerInch p2
