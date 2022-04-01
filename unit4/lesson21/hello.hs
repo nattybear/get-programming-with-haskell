3e34bbae5460a4a4bd5b382e9584bdedf414c7b4
준규 <vpark45@gmail.com>
2022-04-01 11:08:22 +0900

    Solve Summary Q21.1

--- unit4/lesson21/hello.hs
+++ unit4/lesson21/hello.hs
@@ -1,5 +1,10 @@
+import qualified Data.Map as Map
+
 helloPerson :: String -> String
 helloPerson name = "Hello" ++ " " ++ name ++ "!"
+
+nameData :: Map.Map Int String
+nameData = Map.fromList [(1,"Vincent")]
 
 main :: IO ()
 main = do
@@ -7,3 +12,9 @@ main = do
   name <- getLine
   let statement = helloPerson name
   putStrLn statement
+
+maybeMain :: Maybe String
+maybeMain = do
+  name <- Map.lookup 1 nameData
+  let statement = helloPerson name
+  return statement
