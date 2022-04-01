50d055e0d54b268beb7e783ed49385c4a64f40d9
준규 <vpark45@gmail.com>
2022-04-01 10:50:34 +0900

    Add costData

--- unit4/lesson21/pizza.hs
+++ unit4/lesson21/pizza.hs
@@ -25,6 +25,9 @@ describePizza (size,cost) = "The " ++ show size ++ " pizza " ++
 costData :: Map.Map Int Double
 costData = Map.fromList [(1,18.0),(2,16.0)]
 
+sizeData :: Map.Map Int Double
+sizeData = Map.fromList [(1,20.0),(2,15.0)]
+
 main :: IO ()
 main = do
   putStrLn "What is the size of pizza 1"
