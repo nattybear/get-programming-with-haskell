9085c2df71661242cefc8e965745ea98d0478dd9
준규 <vpark45@gmail.com>
2022-04-02 22:23:30 +0900

    Answer Quick check 22.2

--- unit4/lesson22/quick.hs
+++ unit4/lesson22/quick.hs
@@ -2,3 +2,6 @@ exampleMain :: IO ()
 exampleMain = do
   vals <- mapM (\_ -> getLine) [1..3]
   mapM_ putStrLn vals
+
+myReplicateM :: Monad m => Int -> m a -> m [a]
+myReplicateM n func = mapM (\_ -> func) [1 .. n]
