7d831110115aecc6e7b847ab62e976c07abc3a2a
준규 <vpark45@gmail.com>
2022-04-12 07:22:26 +0900

    Search text from the Bhavagad Gita

--- unit4/lesson23/lesson23.hs
+++ unit4/lesson23/lesson23.hs
@@ -59,3 +59,6 @@ unlines' = T.intercalate "\n"
 
 dharma :: T.Text
 dharma = "धर्म"
+
+bgText :: T.Text
+bgText = "श्रेयान्स्वधर्मो विगुणः परधर्मात्स्वनुष्ठितात्।स्वधर्मे निधनं श्रेयः परधर्मो भयावहः"
