a85405bcea2948c3da8ef31104c9e63ecb083389
준규 <vpark45@gmail.com>
2022-04-12 07:32:55 +0900

    Import Data.Text.IO and add main action

--- unit4/lesson23/bg_highlight.hs
+++ unit4/lesson23/bg_highlight.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
 
 dharma :: T.Text
 dharma = "धर्म"
@@ -11,3 +12,6 @@ highlight :: T.Text -> T.Text -> T.Text
 highlight query fullText = T.intercalate highlighted pieces
   where pieces = T.splitOn query fullText
         highlighted = mconcat ["{",query,"}"]
+
+main = do
+  TIO.putStrLn (highlight dharma bgText)
