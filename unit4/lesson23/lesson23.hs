f72bd398473c858cb307f5dde80f893b4baddc52
준규 <vpark45@gmail.com>
2022-04-12 07:24:31 +0900

    Separate file

--- unit4/lesson23/lesson23.hs
+++ unit4/lesson23/lesson23.hs
@@ -56,9 +56,3 @@ lines' = T.splitOn "\n"
 
 unlines' :: [T.Text] -> T.Text
 unlines' = T.intercalate "\n"
-
-dharma :: T.Text
-dharma = "धर्म"
-
-bgText :: T.Text
-bgText = "श्रेयान्स्वधर्मो विगुणः परधर्मात्स्वनुष्ठितात्।स्वधर्मे निधनं श्रेयः परधर्मो भयावहः"
