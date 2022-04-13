1e51f51b7654144b96c46f38bf6e48f1158ac7d0
준규 <vpark45@gmail.com>
2022-04-14 07:40:43 +0900

    Rewrite fileCounts.hs using Text

--- unit4/lesson24/fileCounts.hs
+++ unit4/lesson24/fileCounts.hs
@@ -1,27 +1,30 @@
-import System.Environment
+{-# LANGUAGE OverloadedStrings #-}
+
 import System.IO
+import System.Environment
+import qualified Data.Text as T
+import qualified Data.Text.IO as TI
 
-getCounts :: String -> (Int,Int,Int)
+getCounts :: T.Text -> (Int,Int,Int)
 getCounts input = (charCount, wordCount, lineCount)
-  where charCount = length input
-        wordCount = (length . words) input
-        lineCount = (length . lines) input
+  where charCount = T.length input
+        wordCount = (length . T.words) input
+        lineCount = (length . T.lines) input
 
-countsText :: (Int,Int,Int) -> String
-countsText (cc,wc,lc) = unwords [ "chars: "
-                                , show cc
-                                , " words: "
-                                , show wc
-                                , " Lines: "
-                                , show lc]
+countsText :: (Int,Int,Int) -> T.Text
+countsText (cc,wc,lc) = T.pack (unwords [ "chars: "
+                                        , show cc
+                                        , " words: "
+                                        , show wc
+                                        , " lines: "
+                                        , show lc])
 
 main :: IO ()
 main = do
   args <- getArgs
   let fileName = head args
-  file <- openFile fileName ReadMode
-  input <- hGetContents file
+  input <- TI.readFile fileName
   let summary = (countsText . getCounts) input
-  putStrLn summary
-  hClose file
-  appendFile "stats.dat" (mconcat [fileName, " ", summary, "\n"])
+  TI.appendFile "stats.dat"
+                (mconcat [(T.pack fileName), " ", summary, "\n"])
+  TI.putStrLn summary
