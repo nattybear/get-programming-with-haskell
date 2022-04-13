b9100a91b0478a96e8e81ff12e3983f1d93729b7
준규 <vpark45@gmail.com>
2022-04-13 21:47:53 +0900

    Put code together into main

--- unit4/lesson24/fileCounts.hs
+++ unit4/lesson24/fileCounts.hs
@@ -1,3 +1,6 @@
+import System.Environment
+import System.IO
+
 getCounts :: String -> (Int,Int,Int)
 getCounts input = (charCount, wordCount, lineCount)
   where charCount = length input
@@ -11,3 +14,12 @@ countsText (cc,wc,lc) = unwords [ "chars: "
                                 , show wc
                                 , " Lines: "
                                 , show lc]
+
+main :: IO ()
+main = do
+  args <- getArgs
+  let fileName = head args
+  input <- readFile fileName
+  let summary = (countsText . getCounts) input
+  appendFile "stats.dat" (mconcat [fileName, " ", summary, "\n"])
+  putStrLn summary
