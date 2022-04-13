d3cf021dede9177488e7b85853d1db21388fadc0
준규 <vpark45@gmail.com>
2022-04-13 21:16:53 +0900

    Add countsText function

--- unit4/lesson24/fileCounts.hs
+++ unit4/lesson24/fileCounts.hs
@@ -3,3 +3,11 @@ getCounts input = (charCount, wordCount, lineCount)
   where charCount = length input
         wordCount = (length . words) input
         lineCount = (length . lines) input
+
+countsText :: (Int,Int,Int) -> String
+countsText (cc,wc,lc) = unwords [ "chars: "
+                                , show cc
+                                , " words: "
+                                , show wc
+                                , " Lines: "
+                                , show lc]
