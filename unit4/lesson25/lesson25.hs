fb21a1db18c4dc47b50591a79985df864a28759e
준규 <vpark45@gmail.com>
2022-04-17 08:29:19 +0900

    Answer Quick check 25.3

--- unit4/lesson25/lesson25.hs
+++ unit4/lesson25/lesson25.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+import System.Random
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Char8 as BC
 
@@ -13,3 +14,8 @@ bcInt = "6"
 
 bcToInt :: BC.ByteString -> Int
 bcToInt = read . BC.unpack
+
+randomChar :: IO Char
+randomChar = do
+  randomInt <- randomRIO (0,255)
+  return (toEnum randomInt)
