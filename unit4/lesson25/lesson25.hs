9b972e8e3a09af1354ccd570f6410fb2bbc643c8
준규 <vpark45@gmail.com>
2022-04-17 22:03:59 +0900

    ByteStrings, Char8, and Unicode

--- unit4/lesson25/lesson25.hs
+++ unit4/lesson25/lesson25.hs
@@ -2,6 +2,9 @@
 import System.Random
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Char8 as BC
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+import qualified Data.Text.Encoding as E
 
 sampleBytes :: B.ByteString
 sampleBytes = "Hello!"
@@ -19,3 +22,15 @@ randomChar :: IO Char
 randomChar = do
   randomInt <- randomRIO (0,255)
   return (toEnum randomInt)
+
+fooBC :: BC.ByteString
+fooBC = "준규"
+
+fooText :: T.Text
+fooText = "준규"
+
+fooB :: B.ByteString
+fooB = (BC.pack . T.unpack) fooText
+
+fooSafe :: B.ByteString
+fooSafe = E.encodeUtf8 fooText
