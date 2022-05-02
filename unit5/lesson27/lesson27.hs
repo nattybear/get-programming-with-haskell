403b79f0e4a95b836500741936730e4fb8e4ef57
준규 <vpark45@gmail.com>
2022-05-02 17:09:17 +0900

    Quick check 27.1

--- unit5/lesson27/lesson27.hs
+++ unit5/lesson27/lesson27.hs
@@ -7,3 +7,7 @@ failedRequest = Nothing
 intMaybe :: Maybe Int -> Maybe Int
 intMaybe (Just n) = Just (n + 1)
 intMaybe Nothing = Nothing
+
+reverseMaybe :: Maybe String -> Maybe String
+reverseMaybe (Just s) = Just (reverse s)
+reverseMaybe Nothing = Nothing
