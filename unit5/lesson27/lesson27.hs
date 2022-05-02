3a9f4c7b17d65712a7db9401647205238d56f64f
준규 <vpark45@gmail.com>
2022-05-02 17:41:09 +0900

    Render a RobotPart as HTML

--- unit5/lesson27/lesson27.hs
+++ unit5/lesson27/lesson27.hs
@@ -48,3 +48,17 @@ robotHead = RobotPart
   , cost = 5092.25
   , count = 2
   }
+
+type Html = String
+
+renderHtml :: RobotPart -> Html
+renderHtml part = mconcat ["<h2>",partName,"</h2>"
+                          ,"<p><h2>desc</h2>",partDesc
+                          ,"</p><p><h3>cost</h2>"
+                          ,partCost
+                          ,"</p><p><h3>count</h3>"
+                          ,partCount,"</p>"]
+  where partName = name part
+        partDesc = description part
+        partCost = show (cost part)
+        partCount = show (count part)
