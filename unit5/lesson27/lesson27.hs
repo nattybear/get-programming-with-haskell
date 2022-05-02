e953841b523c191859e17a81c94d1aa7e2d41880
준규 <vpark45@gmail.com>
2022-05-02 21:31:07 +0900

    Summary Q27.1

--- unit5/lesson27/lesson27.hs
+++ unit5/lesson27/lesson27.hs
@@ -91,3 +91,11 @@ leftArmIO = return leftArm
 
 htmlSnippet :: IO Html
 htmlSnippet = renderHtml <$> leftArmIO
+
+data Box a = Box a deriving Show
+
+instance Functor Box where
+  fmap f (Box x) = Box (f x)
+
+morePresents :: Int -> Box a -> Box [a]
+morePresents n box = replicate n <$> box
