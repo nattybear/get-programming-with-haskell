a5933c9ed42653ff17e050c6ed3acf8e2f8a55c7
준규 <vpark45@gmail.com>
2022-05-11 04:22:19 +0900

    Maybe types for the necessary information to create a user

--- unit5/lesson28/game.hs
+++ unit5/lesson28/game.hs
@@ -3,3 +3,12 @@ data User = User
   , gamerId :: Int
   , score :: Int
   } deriving Show
+
+serverUsername :: Maybe String
+serverUsername = Just "Sue"
+
+serverGamerId :: Maybe Int
+serverGamerId = Just 1337
+
+serverScore :: Maybe Int
+serverScore = Just 9001
