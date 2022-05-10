d3a835e0f9ddd682ab416514d09794cc3d4b3368
준규 <vpark45@gmail.com>
2022-05-11 02:26:13 +0900

    The main for dist.hs

--- unit5/lesson28/dist.hs
+++ unit5/lesson28/dist.hs
@@ -40,3 +40,14 @@ addMaybe (Just x) (Just y) = Just (x + y)
 addMaybe _ _ = Nothing
 
 maybeInc = (+) <$> Just 1
+
+main :: IO ()
+main = do
+  putStrLn "Enter the starting city name:"
+  startingInput <- getLine
+  let startingCity = Map.lookup startingInput locationDB
+  putStrLn "Enter the destination city name:"
+  destInput <- getLine
+  let destCity = Map.lookup destInput locationDB
+  let distance = haversine <$> startingCity <*> destCity
+  printDistance distance
