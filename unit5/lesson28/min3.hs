a8f0d262c2ca2c76b4d54fae3f965bb9dcd0dba0
준규 <vpark45@gmail.com>
2022-05-11 03:11:30 +0900

    minOfInts shows using multiple arguments with <*>

--- unit5/lesson28/min3.hs
+++ unit5/lesson28/min3.hs
@@ -3,3 +3,6 @@ minOfThree val1 val2 val3 = min val1 (min val2 val3)
 
 readInt :: IO Int
 readInt = read <$> getLine
+
+minOfInts :: IO Int
+minOfInts = minOfThree <$> readInt <*> readInt <*> readInt
