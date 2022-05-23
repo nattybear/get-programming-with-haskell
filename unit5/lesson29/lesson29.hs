186164f13018451a666a252d3f95da4cfc1fdcec
Vincent <vpark45@gmail.com>
2022-05-23 22:36:11 +0000

    Add User type

--- unit5/lesson29/lesson29.hs
+++ unit5/lesson29/lesson29.hs
@@ -6,3 +6,15 @@ boxPrize = [500,20000]
 
 totalPrize :: [Int]
 totalPrize = pure (+) <*> doorPrize <*> boxPrize
+
+primesToN :: Integer -> [Integer]
+primesToN n = filter isNotComposite twoThroughN
+  where twoThroughN = [2 .. n]
+        composite = pure (*) <*> twoThroughN <*> twoThroughN
+        isNotComposite = not . (`elem` composite)
+
+data User = User {
+    name    :: String
+  , gamerId :: Int
+  , score   :: Int
+} deriving Show
