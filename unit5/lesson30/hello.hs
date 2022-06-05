faa54408bc947cd2ead455830e14ad74af8a06a8
Vincent <vpark45@gmail.com>
2022-06-05 17:49:18 +0000

    Hello Name program using Monad method

--- unit5/lesson30/hello.hs
+++ unit5/lesson30/hello.hs
@@ -3,3 +3,10 @@ askForName = putStrLn "What is your name?"
 
 nameStatement :: String -> String
 nameStatement name = "Hello, " ++ name ++ "!"
+
+helloName :: IO ()
+helloName = askForName >>
+            getLine >>=
+            (\name ->
+              return (nameStatement name)) >>=
+            putStrLn
