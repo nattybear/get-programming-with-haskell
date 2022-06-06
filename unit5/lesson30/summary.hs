5e63e90405e1a2143996c71ef9de81281bbfedf5
Vincent <vpark45@gmail.com>
2022-06-06 07:42:48 +0000

    Implement bind for Maybe

--- unit5/lesson30/summary.hs
+++ unit5/lesson30/summary.hs
@@ -3,3 +3,7 @@ allFmapM f ma = ma >>= (\x -> return $ f x)
 
 allApp :: Monad m => m (a -> b) -> m a -> m b
 allApp mf mx = mf >>= (\f -> mx >>= (\x -> return (f x)))
+
+bind :: Maybe a -> (a -> Maybe b) -> Maybe b
+bind Nothing  _ = Nothing
+bind (Just x) f = f x
