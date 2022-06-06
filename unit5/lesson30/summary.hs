ae794abc60b9e351e5bda9cd6e6990319237aed4
Vincent <vpark45@gmail.com>
2022-06-06 07:37:37 +0000

    Implement <*> using >>=

--- unit5/lesson30/summary.hs
+++ unit5/lesson30/summary.hs
@@ -1,2 +1,5 @@
 allFmapM :: Monad m => (a -> b) -> m a -> m b
 allFmapM f ma = ma >>= (\x -> return $ f x)
+
+allApp :: Monad m => m (a -> b) -> m a -> m b
+allApp mf mx = mf >>= (\f -> mx >>= (\x -> return (f x)))
