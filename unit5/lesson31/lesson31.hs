0ee6511b1402a6a79c11e1f852b32010ece76be8
준규 <vpark45@gmail.com>
2022-06-25 21:45:38 +0900

    Add maxPairM function

--- unit5/lesson31/lesson31.hs
+++ unit5/lesson31/lesson31.hs
@@ -10,3 +10,8 @@ helloName = askForName >>
             (\name ->
               return (nameStatement name)) >>=
             putStrLn
+
+maxPairM :: (Monad m, Ord a) => m (a, a) -> m a
+maxPairM pair = do
+  (x, y) <- pair
+  return (max x y)
