819649d014362197fb2a13ead4e7f3dab7f7b2e4
준규 <vpark45@gmail.com>
2022-06-25 23:35:02 +0900

    Add Candidate data type

--- unit5/lesson31/job.hs
+++ unit5/lesson31/job.hs
@@ -1,3 +1,9 @@
 data Grade = F | D | C | B | A deriving (Eq, Ord, Enum, Show, Read)
 
 data Degree = HS | BA | MS | PhD deriving (Eq, Ord, Enum, Show, Read)
+
+data Candidate = Candidate
+  { candidateId :: Int
+  , codeReview  :: Grade
+  , cultureFit  :: Grade
+  , education   :: Degree } deriving Show
