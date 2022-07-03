b7451e498e6d9962a19ee04611819ae14186cf19
Vincent <vpark45@gmail.com>
2022-07-03 14:18:34 +0000

    Add some functions

--- unit5/lesson32/lesson32.hs
+++ unit5/lesson32/lesson32.hs
@@ -1,2 +1,17 @@
 considerThis :: [Int]
 considerThis = [ x | x <- [1..19], odd x ]
+
+powersOfTwo :: Int -> [Int]
+powersOfTwo n = do
+  value <- [1 .. n]
+  return (2^value)
+
+powersOfTwoMap :: Int -> [Int]
+powersOfTwoMap n = map (\x -> 2^x) [1 .. n]
+
+powersOfTwoAndThree :: Int -> [(Int, Int)]
+powersOfTwoAndThree n = do
+  value <- [1 .. n]
+  let powersOfTwo   = 2^value
+      powersOfThree = 3^value
+  return (powersOfTwo, powersOfThree)
