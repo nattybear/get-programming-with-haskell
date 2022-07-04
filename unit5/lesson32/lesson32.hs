8d6d57ab302cb7fa645d514ad9ee679600e6d331
Vincent <vpark45@gmail.com>
2022-07-04 22:31:38 +0000

    Rewrite using list comprehension

--- unit5/lesson32/lesson32.hs
+++ unit5/lesson32/lesson32.hs
@@ -4,19 +4,16 @@ considerThis :: [Int]
 considerThis = [ x | x <- [1..19], odd x ]
 
 powersOfTwo :: Int -> [Int]
-powersOfTwo n = do
-  value <- [1 .. n]
-  return (2^value)
+powersOfTwo n = [value^2 | value <- [1 .. n]]
 
 powersOfTwoMap :: Int -> [Int]
 powersOfTwoMap n = map (\x -> 2^x) [1 .. n]
 
 powersOfTwoAndThree :: Int -> [(Int, Int)]
-powersOfTwoAndThree n = do
-  value <- [1 .. n]
-  let powersOfTwo   = 2^value
-      powersOfThree = 3^value
-  return (powersOfTwo, powersOfThree)
+powersOfTwoAndThree n = [(powersOfTwo,powersOfThree)
+                        | value <- [1 .. n]
+                        , let powersOfTwo = 2^value
+                        , let powersOfThree = 3^value]
 
 allEvenOdds :: Int -> [(Int,Int)]
 allEvenOdds n = do
