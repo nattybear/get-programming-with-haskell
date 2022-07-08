00805e1f23dd03fa7631a929dd4c683455936237
Vincent <vpark45@gmail.com>
2022-07-08 14:39:05 +0000

    Solve Summary Q32.1 Q32.2

--- unit5/lesson32/lesson32.hs
+++ unit5/lesson32/lesson32.hs
@@ -47,3 +47,22 @@ answer :: [String]
 answer = ["Mr. " ++ capVal | val <- colors
                            , let capVal = (\(x:xs) ->
                                             toUpper x:xs) val]
+
+monthEnds :: [Int]
+monthEnds = [31,28,31,30,31,30,31,31,30,31,30,31]
+
+dates :: [Int] -> [Int]
+dates ends = [ date | end <- ends, date <- [1..end] ]
+
+datesDo :: [Int] -> [Int]
+datesDo ends = do
+  end <- ends
+  date <- [1..end]
+  return date
+
+datesMonad :: [Int] -> [Int]
+datesMonad ends = ends >>=
+  (\end ->
+    [1..end] >>=
+      (\date -> return date))
+
