0874a98bb85bca6d286035a7a96b47f8612a760d
Vincent <vpark45@gmail.com>
2022-07-17 22:12:47 +0000

    Add Palindrome module and refactor Main

--- unit6/lesson34/Main.hs
+++ unit6/lesson34/Main.hs
@@ -1,5 +1,7 @@
 module Main where
 
+import qualified Palindrome
+
 isPalindrome :: String -> Bool
 isPalindrome text = text == reverse text
 
@@ -7,7 +9,7 @@ main :: IO ()
 main = do
   print "Enter a word and I'll let you know if it's a palindrome!"
   text <- getLine
-  let response = if isPalindrome text
+  let response = if Palindrome.isPalindrome text
                  then "it is!"
                  else "it's not!"
   print response
