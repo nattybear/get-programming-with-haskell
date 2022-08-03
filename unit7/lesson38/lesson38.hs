3d37a5348eac8d5904fa0684f227dd7bc11089e7
Vincent <vpark45@gmail.com>
2022-08-03 22:28:42 +0000

    Delete myTake and solve Q38.1

--- unit7/lesson38/lesson38.hs
+++ unit7/lesson38/lesson38.hs
@@ -1,3 +1,19 @@
-myTake :: Int -> [a] -> [a]
-myTake 0 _  = []
-myTake n xs = head xs : myTake (n-1) (tail xs)
+import Data.Char (isDigit)
+
+data ParseError = First | Second | Neither
+
+instance Show ParseError where
+  show First   = "First value can't be parsed"
+  show Second  = "Second value can't be parsed"
+  show Neither = "Neither value can be parsed"
+
+addStrInts :: String -> String -> Either ParseError Int
+addStrInts s1 s2
+  = case (isNumber s1, isNumber s2) of
+      (True,  True)  -> Right $ read s1 + read s2
+      (False, True)  -> Left First
+      (True,  False) -> Left Second
+      _              -> Left Neither
+
+isNumber :: String -> Bool
+isNumber = all isDigit
